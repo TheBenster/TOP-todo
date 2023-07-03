c850ce3fdee3c8f1fac787ff932d5db1ecac45b4
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-07-03 12:14:29 -0400

    change sidebar functionality to projects instead of tasks, clean up format and add comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import "./index.css";
-import { addTaskToList } from "./todos";
+import {
+  addProjectToList,
+  addTaskToList,
+  loadProjectsFromStorage,
+  saveProjectsToLocalStorage,
+  delProj,
+} from "./todos";
 
 const taskBtn = document.querySelector("#task-add");
 const closeBtn = document.querySelector("#close-modal");
 const modal = document.querySelector(".modal");
 const submitBtn = document.querySelector("#submit");
+const newProj = document.querySelector("#new-proj");
+const projModal = document.querySelector(".projModal");
+const submitProj = document.querySelector("#submitProj");
+const cancelProj = document.querySelector("#cancelProj");
+const trashIcon = document.querySelector(".fas.fa-trash-alt");
 
 function openModal() {
   modal.setAttribute("open", "true");
@@ -21,4 +32,30 @@ submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
   addTaskToList();
   closeModal();
+});
+
+function closeProjModal() {
+  projModal.removeAttribute("open");
+}
+
+// appends project to ul and eventually will assign #selected to the current project
+newProj.addEventListener("click", openOtherModal);
+cancelProj.addEventListener("click", closeProjModal);
+
+submitProj.addEventListener("click", (event) => {
+  event.preventDefault();
+  addProjectToList();
+  closeProjModal();
+});
+
+trashIcon.addEventListener("click", delProj);
+function openOtherModal() {
+  projModal.setAttribute("open", "true");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadProjectsFromStorage();
+});
+window.addEventListener("beforeunload", () => {
+  saveProjectsToLocalStorage();
 });
